cb5c6d4d844a2e8d6b17dc6dfd2c46a42d357752
Dev-Moj <mojtabagafarzade@gmail.com>
2024-06-03 19:04:50 +0330

    fix error handling

--- main.py
+++ main.py
@@ -27,13 +27,16 @@ async def main():
         if dialog.is_user:
             entity = dialog.entity
             async for message in client.iter_messages(entity, offset_date=datetime.now(), limit=20):
-                if message.file and message.media.ttl_seconds:
-                    fname = f'{message.id}.{message.file.mime_type.split("/")[1]}'
-                    if not os.path.exists(fname):
-                        print('Downloading...')
-                        await client.download_media(message.media, fname)
-                        await client.send_file('me', open(fname, 'rb'))
-                        print('done')
+                try:
+                    if message.file and message.media.ttl_seconds:
+                        fname = f'{message.id}.{message.file.mime_type.split("/")[1]}'
+                        if not os.path.exists(fname):
+                            print('Downloading...')
+                            await client.download_media(message.media, fname)
+                            await client.send_file('me', open(fname, 'rb'))
+                            print('done')
+                except Exception as e:
+                    pass
 
 
 if __name__ == '__main__':
